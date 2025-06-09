78d9619e71370d59e05b8cec913470f527ee9ebe
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-06-09 20:51:42 -0300

    fix: correção no data sql

---
+++ src/main/resources/data-H2.sql
@@ -0,0 +1,4 @@
+
+INSERT  into  Papel (id,autoridade) VALUES ( 1,'aluno');
+INSERT  into  Papel (id,autoridade) VALUES ( 2,'professor');
+INSERT  into  papel (id,autoridade) VALUES ( 3,'administrador');
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 
-INSERT into  papel (id,autoridade) VALUES ( 1,'usuario');
-INSERT into  papel (id,autoridade) VALUES ( 2,'proprietario');
-INSERT into  papel (id,autoridade) VALUES ( 3,'administrador');
+INSERT  into  Papel (id,autoridade) VALUES ( 1,'aluno') ON CONFLICT (id) DO NOTHING;
+
+INSERT  into  Papel (id,autoridade) VALUES ( 2,'professor') ON CONFLICT (id) DO NOTHING;
+
+INSERT  into  papel (id,autoridade) VALUES ( 3,'administrador') ON CONFLICT (id) DO NOTHING;
